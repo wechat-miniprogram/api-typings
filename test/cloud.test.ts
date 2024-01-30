398acc8cb2cef3449dfe1219b2f4b85ffff14e0b
missanni <missannil@163.com>
2024-01-30 08:35:59 +0800

    fix: 类型补全 runTransaction aggregate

--- test/cloud.test.ts
+++ test/cloud.test.ts
@@ -1,14 +1,12 @@
 
 
-//补全[文档](https://developers.weixin.qq.com/miniprogram/dev/wxcloud/reference-sdk-api/constant/constant.html#%E7%A4%BA%E4%BE%8B)中DYNAMIC_CURRENT_ENV类型
 
 const cloud = wx.cloud
 const db = cloud.database()
+const _ = db.command
+// https://developers.weixin.qq.com/miniprogram/dev/wxcloud/reference-sdk-api/constant/constant.html#%E7%A4%BA%E4%BE%8B
 cloud.init({
-  env:cloud['DYNAMIC_CURRENT_ENV']
-})
-cloud.database({
-  env:cloud['DYNAMIC_CURRENT_ENV']
+  env:cloud.DYNAMIC_CURRENT_ENV
 })
 cloud.callFunction({
   name: 'getInfo',
@@ -16,3 +14,20 @@ cloud.callFunction({
 	  env: cloud.DYNAMIC_CURRENT_ENV
   },
 })
+
+//  https://developers.weixin.qq.com/miniprogram/dev/wxcloud/reference-sdk-api/Cloud.database.html
+cloud.database({
+  throwOnNotFound:true
+})
+
+
+//  https://developers.weixin.qq.com/miniprogram/dev/wxcloud/reference-sdk-api/database/Database.runTransaction.html
+
+db.runTransaction(async transaction => {transaction})
+
+// https://developers.weixin.qq.com/miniprogram/dev/wxcloud/reference-sdk-api/database/aggregate/Aggregate.html
+db.collection('')
+  .aggregate()
+  .match({})
+  .lookup({})
+  .end()
